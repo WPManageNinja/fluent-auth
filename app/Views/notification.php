0dfd86ceade1f1510bd7bf7e424fa46de08f210e
tdrayson <33532995+tdrayson@users.noreply.github.com>
2024-05-16 14:39:05 +0100

    Renamed footer plugin name to keep consistent naming convention

--- app/Views/notification.php
+++ app/Views/notification.php
@@ -132,7 +132,7 @@
                     <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
                         <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">This email has been sent from Fluent Login & Security Plugin. Site: <?php echo esc_url(site_url()); ?></span>
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">This email has been sent from FluentAuth Security Plugin. Site: <?php echo esc_url(site_url()); ?></span>
                             </td>
                         </tr>
                     </table>
